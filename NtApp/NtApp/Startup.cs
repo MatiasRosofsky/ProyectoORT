4eb3320bac6cb1306e1db16402c25cc71c51b94d
Matías Rosofsky <rosofskyort@gmail.com>
2018-11-22 21:32:21 -0300

    Primera subida

---
+++ NtApp/NtApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NtApp.Startup))]
+namespace NtApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
